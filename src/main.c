66baaa7cae37d80f607e9078120ce3c30799c6ad
italo <riositalo2@gmail.com>
2026-02-04 10:00:46 -0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -3,17 +3,19 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-  // Flush after every printf
-  setbuf(stdout, NULL);
+  while (1) {
+    // Flush after every printf
+    setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
+    // TODO: Uncomment the code below to pass the first stage
+    printf("$ ");
 
-  char command[1024];
-  fgets(command, sizeof(command), stdin);
-  // Deletes the \n char present in fgets (since the input is recieved when the
-  // user presses \n, it needs to be deleted)
-  command[strcspn(command, "\n")] = '\0';
-  printf("%s: command not found\n", command);
+    char command[1024];
+    fgets(command, sizeof(command), stdin);
+    // Deletes the \n char present in fgets (since the input is recieved when
+    // the user presses \n, it needs to be deleted)
+    command[strcspn(command, "\n")] = '\0';
+    printf("%s: command not found\n", command);
+  }
   return 0;
 }
