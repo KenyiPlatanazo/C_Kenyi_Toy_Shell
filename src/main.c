50659507c74bd63774f1469382f48ab38a221d02
italo <riositalo2@gmail.com>
2026-02-16 17:59:09 -0500

    Refactored the architecture for the processing of commands in general. The previous impleemntation was complete shit. My bad twin

--- src/main.c
+++ src/main.c
@@ -5,12 +5,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
-enum Command { SHELL_UNKNOWN, SHELL_ECHO, SHELL_EXIT, SHELL_TYPE };
+#define WHITESPACE ' '
+enum Command {
+  SHELL_UNKNOWN,
+  SHELL_ECHO,
+  SHELL_EXIT,
+  SHELL_TYPE,
+  SHELL_EXEC_FILE
+};
+void process_command(char *command);
+void dispatch(int argc, char *argv[]);
 enum Command parse_command(const char *command);
 bool does_exec_exists(const char *command, const char *dir);
 void check_type(const char *command);
 void process_type(const char *command);
+void exec_file(const char *dir);
+int tokenize(char *line, char *argv[]);
 
 int main(int argc, char *argv[]) {
   while (1) {
@@ -23,30 +33,18 @@ int main(int argc, char *argv[]) {
     // Deletes the \n char present in fgets (since the input is recieved when
     // the user presses \n, it needs to be deleted)
     command[strcspn(command, "\n")] = '\0';
-    switch (parse_command(command)) {
-    case SHELL_EXIT:
-      exit(0);
-      break;
-    case SHELL_ECHO:
-      printf("%s\n", command + 5);
-      break;
-    case SHELL_TYPE:
-      process_type(command);
-      break;
-    case SHELL_UNKNOWN:
-      printf("%s: command not found\n", command);
-      break;
-    }
+    process_command(command);
   }
   return 0;
 }
-
 enum Command parse_command(const char *command) {
+  if (strncmp(command, "./", 2) == 0)
+    return SHELL_EXEC_FILE;
   if (strcmp(command, "exit") == 0)
     return SHELL_EXIT;
-  if (strncmp(command, "echo", 4) == 0)
+  if (strcmp(command, "echo") == 0)
     return SHELL_ECHO;
-  if (strncmp(command, "type", 4) == 0)
+  if (strcmp(command, "type") == 0)
     return SHELL_TYPE;
   return SHELL_UNKNOWN;
 }
@@ -85,6 +83,24 @@ bool does_exec_exists(const char *command, const char *dir) {
   return result;
 }
 
+void handle_type(int argc, char *argv[]) {
+  if (argc < 2)
+    return;
+  for (int i = 1; i < argc; i++) {
+    check_type(argv[i]);
+  }
+}
+
+void handle_echo(int argc, char *argv[]) {
+  for (int i = 1; i < argc; i++) {
+    printf("%s", argv[i]);
+    if (i < argc - 1) {
+      printf(" ");
+    }
+  }
+  printf("\n");
+}
+
 void process_type(const char *command) {
   char *cmd_name = strdup(command + 5);
   char *inner_saveptr = NULL;
@@ -101,4 +117,44 @@ void process_type(const char *command) {
     token = strtok_r(NULL, " ", &inner_saveptr);
   }
   free(cmd_name);
+}
+
+void process_command(char *command) {
+  char *argv[1024];
+  int argc = tokenize(command, argv);
+  if (argc == 0)
+    return;
+  dispatch(argc, argv);
+}
+
+int tokenize(char *line, char *argv[]) {
+  int argc = 0;
+  char *save_ptr = NULL;
+  char *token = strtok_r(line, " ", &save_ptr);
+  while (token != NULL) {
+    argv[argc++] = token;
+    token = strtok_r(NULL, " ", &save_ptr);
+  }
+  argv[argc] = NULL;
+  return argc;
+}
+
+void dispatch(int argc, char *argv[]) {
+  enum Command command = parse_command(argv[0]);
+  switch (command) {
+  case SHELL_EXIT:
+    exit(0);
+    break;
+  case SHELL_ECHO:
+    handle_echo(argc, argv);
+    break;
+  case SHELL_TYPE:
+    handle_type(argc, argv);
+    break;
+  case SHELL_EXEC_FILE:
+    break;
+  case SHELL_UNKNOWN:
+    printf("%s: command not found\n", argv[0]);
+    break;
+  }
 }
