dd335e9fa322d74fcebafb1b458050d41fe03bfe
italo <riositalo2@gmail.com>
2026-02-28 20:37:58 -0500

    codecrafters submit [skip ci]

--- src/parser.c
+++ src/parser.c
@@ -306,7 +306,6 @@ void parse_tokens(struct t_pipeline *pipeline, int argc,
       struct raw_token *next = argv[i + 1];
       process_redir(&pipeline->commands[current_cmd].redirs[redir_index++],
                     argv[i], next->value, DEFAULT_FD);
-      pipeline->commands[current_cmd].redir_count++;
       // We skip over the filename as to not take as an argv for the command
       i++;
       break;
